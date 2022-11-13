3355c3c7cac54cc21321e219c3ed4e03049aa4c8
ssumthingood <ssumthingood@gmail.com>
2022-11-13 14:46:01 +0900

    1113

--- 42885.js
+++ 42885.js
@@ -3,16 +3,19 @@
 function solution(people, limit) {
     var answer = 0;
     people.sort((a, b) => a - b);
+    //일단 무게순으로 정렬
     console.log(people);
     while (people.length > 0) {
         var n = people.length;
         if (people[n - 1] + people[0] > limit) {
             people.pop();
             answer++;
+            //가장 큰놈과 작은놈 합친 값이 무게초과면 큰놈만 태워보냄
         } else {
             people.shift();
             people.pop();
             answer++;
+            //큰놈과 작은놈 무게 합이 무게기준을 만족하면 두명 태워보냄
         }
     }
     return answer;
