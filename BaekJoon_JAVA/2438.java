5400678f29c35ba3b8f9657d4e79928403c729e8
ssumthingood <ssumthingood@gmail.com>
2023-01-12 22:50:23 +0900

    20230112

---
+++ BaekJoon_JAVA/2438.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String args[]) throws IOException{
+        // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // StringTokenizer st = new StringTokenizer(br.readLine());
+        // int T = Integer.parseInt(st.nextToken());
+        int T = 5;
+        StringBuilder sb = new StringBuilder();
+        sb.append("");
+            
+        for(int i=1; i<T+1;i++){
+            for(int j=1;j<i+1;j++){
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
