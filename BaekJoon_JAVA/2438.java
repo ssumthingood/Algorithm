85934bc14e144962fdd99d1f29157031c2261044
ssumthingood <ssumthingood@gmail.com>
2023-01-14 11:38:22 +0900

    20230114

--- BaekJoon_JAVA/2438.java
+++ BaekJoon_JAVA/2438.java
@@ -3,10 +3,9 @@ import java.util.*;
 
 public class Main{
     public static void main(String args[]) throws IOException{
-        // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        // StringTokenizer st = new StringTokenizer(br.readLine());
-        // int T = Integer.parseInt(st.nextToken());
-        int T = 5;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int T = Integer.parseInt(st.nextToken());
         StringBuilder sb = new StringBuilder();
         sb.append("");
             
