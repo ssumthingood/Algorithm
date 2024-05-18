154b9a73de3df0bfea498aad83847f056896407c
ssumthingood <ssumthingood@gmail.com>
2024-05-18 16:29:49 +0900

    0518

--- BaekJoon_JAVA/17807.java
+++ BaekJoon_JAVA/17807.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Baek_17087 {
+public class Main {
     static int N;
     static int S;
 
